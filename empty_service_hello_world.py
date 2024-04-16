66de36860de2b4933143fca26fd53537514437a8
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-04-16 12:32:13 +0300

    two_paths_with_numbers

--- empty_service_hello_world.py
+++ empty_service_hello_world.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 srv = FastAPI()
 
-@srv.get("/")
-async def root():
-    return {"message" : "Hello, World!"}
+@srv.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
 
+
+@srv.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}

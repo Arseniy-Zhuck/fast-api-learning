77d7e74140fbc05229e49138963ace9a0adef6cd
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-04-16 12:59:10 +0300

    parametr_str

--- empty_service_hello_world.py
+++ empty_service_hello_world.py
@@ -1,12 +1,16 @@
+from enum import Enum
 from fastapi import FastAPI
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 srv = FastAPI()
 
-@srv.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@srv.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@srv.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}

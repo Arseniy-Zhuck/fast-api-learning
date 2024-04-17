40cc30e738e2f82520c4a21ba3b46d6a288b17e1
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-04-17 09:14:47 +0300

    post_query_1st_attempt

--- empty_service_hello_world.py
+++ empty_service_hello_world.py
@@ -1,11 +1,36 @@
+from __future__ import annotations
+
+from ctypes import Union
 from enum import Enum
 from fastapi import FastAPI
+
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 srv = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "ars"}]
+
+@srv.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@srv.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
 
 @srv.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -14,3 +39,28 @@ async def get_model(model_name: ModelName):
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@srv.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@srv.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
